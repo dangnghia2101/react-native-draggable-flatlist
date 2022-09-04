4ae677c7b05209f804d9dfffb4896e6d7e5fcef4
DangNghia21 <dangnghia2101@gmail.com>
2022-09-04 20:01:36 +0700

    Revert "Revert "update: run on UI""
    This reverts commit 559a1cf2e1ca212d44c074b056443a671d09b74b.

--- src/components/DraggableFlatList.tsx
+++ src/components/DraggableFlatList.tsx
@@ -14,6 +14,7 @@ import {
 } from "react-native-gesture-handler";
 import Animated, {
   runOnJS,
+  runOnUI,
   useAnimatedReaction,
   useAnimatedScrollHandler,
   useSharedValue,
@@ -317,7 +318,7 @@ function DraggableFlatListInner<T>(props: DraggableFlatListProps<T>) {
         scrollOffset.value = horizontalAnim.value
           ? evt.contentOffset.x
           : evt.contentOffset.y;
-        runOnJS(onScroll)(scrollOffset.value);
+        runOnUI(onScroll)(scrollOffset.value);
       },
     },
     [horizontalAnim]
